5a8d487ca9c87ac31f88e33225cca3fac9838d91
Vinermy <egorkosachev@vk.com>
2022-08-12 16:31:02 +0700

    Comments in model.py changed

--- model.py
+++ model.py
@@ -15,14 +15,14 @@ def calculate_forces(body: celestialBody, other: list):
         direction = other_body.position - body.position                      #Vector connecting centers of affecting and affected body
         force = G * ((body.mass * other_body.mass) / sqr(direction.length))  #Numeric value of gravitational force
         acceleration = force / body.mass
-        direction.stretch(acceleration)                                      #direction.length is now equal to numeric value of gravitational force
+        direction.stretch(acceleration)                                      #direction now represents the acceleration
         body.affected_by.append((direction.x, direction.y))
     
 def apply_forces(body: celestialBody):
-    summ = vector2d(0, 0)          #Resulting force's vector representation
-    for force in body.affected_by: #Summ all the forces that body is affected by
+    summ = vector2d(0, 0)          #Resulting acceleration's vector representation
+    for force in body.affected_by: #Summ all the accelerations that body is affected by
         summ += force              
-    body.speed += summ             #Add the resulting force to current speed
+    body.speed += summ             #Add the resulting acceleration to current speed
     body.position += body.speed    #Move the body accordinly
 
 def calculate_step(bodies: list): #Update the simulation
