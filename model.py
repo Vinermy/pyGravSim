664f5d2f299f7e2e8db044c7bfc32963b0ff6951
Vinermy <egorkosachev@vk.com>
2022-08-12 16:25:08 +0700

    modified main model file

--- model.py
+++ model.py
@@ -6,13 +6,28 @@ import configparser as cp
 config = cp.ConfigParser()
 config.read("config.ini")
 
-G = float(config["CONSTANTS"]["G"])*(10**-11)
+G = float(config["CONSTANTS"]["G"])*(10**-11) #Gravitational constant
 
 def calculate_forces(body: celestialBody, other: list):
+    body.affected_by = []
     for other_body in other:
         assert type(other_body) == celestialBody, "All objects in the list must be exemplars of 'celestialBody' class"
-        direction = other_body.position - body.position
-        force = G * ((body.mass * other_body.mass) / sqr(direction.length))
-        direction = direction.normalize()
+        direction = other_body.position - body.position                      #Vector connecting centers of affecting and affected body
+        force = G * ((body.mass * other_body.mass) / sqr(direction.length))  #Numeric value of gravitational force
         acceleration = force / body.mass
-        
+        direction.stretch(acceleration)                                      #direction.length is now equal to numeric value of gravitational force
+        body.affected_by.append((direction.x, direction.y))
+    
+def apply_forces(body: celestialBody):
+    summ = vector2d(0, 0)          #Resulting force's vector representation
+    for force in body.affected_by: #Summ all the forces that body is affected by
+        summ += force              
+    body.speed += summ             #Add the resulting force to current speed
+    body.position += body.speed    #Move the body accordinly
+
+def calculate_step(bodies: list): #Update the simulation
+    for body in bodies:
+        calculate_forces(body=body, other=bodies)
+    for body in bodies:
+        apply_forces(body=body)
+    
