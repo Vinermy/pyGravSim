14247c7cd142ba2d57635adbcf12f085f1cf46d5
Vinermy <egorkosachev@vk.com>
2022-09-22 18:40:08 +0700

    Modified min/max values of the spinboxes

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-# Form generated from reading UI file 'MainWindow.ui'
+## Form generated from reading UI file 'mainwindow.ui'
 ##
-# Created by: Qt User Interface Compiler version 6.3.1
+## Created by: Qt User Interface Compiler version 6.3.1
 ##
-# WARNING! All changes made in this file will be lost when recompiling UI file!
+## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-                            QMetaObject, QObject, QPoint, QRect,
-                            QSize, QTime, QUrl, Qt)
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-                           QFont, QFontDatabase, QGradient, QIcon,
-                           QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QGridLayout,
-                               QGroupBox, QHBoxLayout, QLabel, QLayout,
-                               QListWidget, QListWidgetItem, QMainWindow, QPushButton,
-                               QSizePolicy, QSlider, QSpinBox, QVBoxLayout,
-                               QWidget)
+    QGroupBox, QHBoxLayout, QLabel, QLayout,
+    QListWidget, QListWidgetItem, QMainWindow, QPushButton,
+    QSizePolicy, QSlider, QSpinBox, QVBoxLayout,
+    QWidget)
 import resource_rc
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -47,8 +46,7 @@ class Ui_MainWindow(object):
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.lstBodies.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.lstBodies.sizePolicy().hasHeightForWidth())
         self.lstBodies.setSizePolicy(sizePolicy)
 
         self.vloToolbar.addWidget(self.gpbBodies)
@@ -113,16 +111,19 @@ class Ui_MainWindow(object):
         self.spbSpdX = QSpinBox(self.verticalLayoutWidget)
         self.spbSpdX.setObjectName(u"spbSpdX")
         self.spbSpdX.setEnabled(True)
-        self.spbSpdX.setMinimum(-310)
-        self.spbSpdX.setMaximum(310)
+        self.spbSpdX.setMinimum(-500)
+        self.spbSpdX.setMaximum(500)
 
         self.gloPosSpd.addWidget(self.spbSpdX, 1, 2, 1, 1)
 
         self.spbSpdY = QSpinBox(self.verticalLayoutWidget)
         self.spbSpdY.setObjectName(u"spbSpdY")
         self.spbSpdY.setEnabled(True)
+        self.spbSpdY.setMinimum(-500)
+        self.spbSpdY.setMaximum(500)
 
         self.gloPosSpd.addWidget(self.spbSpdY, 1, 4, 1, 1)
+
 
         self.vloEdit.addLayout(self.gloPosSpd)
 
@@ -136,9 +137,12 @@ class Ui_MainWindow(object):
         self.spbMass = QSpinBox(self.verticalLayoutWidget)
         self.spbMass.setObjectName(u"spbMass")
         self.spbMass.setEnabled(True)
+        self.spbMass.setMinimum(1)
+        self.spbMass.setMaximum(1000000)
         self.spbMass.setValue(1)
 
         self.hloMass.addWidget(self.spbMass)
+
 
         self.vloEdit.addLayout(self.hloMass)
 
@@ -152,9 +156,12 @@ class Ui_MainWindow(object):
         self.spbRadius = QSpinBox(self.verticalLayoutWidget)
         self.spbRadius.setObjectName(u"spbRadius")
         self.spbRadius.setEnabled(True)
+        self.spbRadius.setMinimum(1)
+        self.spbRadius.setMaximum(100)
         self.spbRadius.setValue(10)
 
         self.hloRadius.addWidget(self.spbRadius)
+
 
         self.vloEdit.addLayout(self.hloRadius)
 
@@ -179,9 +186,12 @@ class Ui_MainWindow(object):
 
         self.hloButtons.addWidget(self.btnCopy)
 
+
         self.vloButtons.addLayout(self.hloButtons)
 
+
         self.vloEdit.addLayout(self.vloButtons)
+
 
         self.vloToolbar.addWidget(self.gpbEdit)
 
@@ -190,7 +200,7 @@ class Ui_MainWindow(object):
         self.gpbSettings.setMaximumSize(QSize(255, 150))
         self.verticalLayoutWidget_3 = QWidget(self.gpbSettings)
         self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
-        self.verticalLayoutWidget_3.setGeometry(QRect(9, 19, 231, 123))
+        self.verticalLayoutWidget_3.setGeometry(QRect(9, 19, 231, 127))
         self.vloSimSettings = QVBoxLayout(self.verticalLayoutWidget_3)
         self.vloSimSettings.setObjectName(u"vloSimSettings")
         self.vloSimSettings.setContentsMargins(0, 0, 0, 0)
@@ -214,15 +224,16 @@ class Ui_MainWindow(object):
         self.hrsSimSpd = QSlider(self.verticalLayoutWidget_3)
         self.hrsSimSpd.setObjectName(u"hrsSimSpd")
         self.hrsSimSpd.setMinimum(1)
-        self.hrsSimSpd.setMaximum(9)
-        self.hrsSimSpd.setValue(5)
+        self.hrsSimSpd.setMaximum(61)
+        self.hrsSimSpd.setValue(31)
         self.hrsSimSpd.setOrientation(Qt.Horizontal)
         self.hrsSimSpd.setInvertedAppearance(False)
         self.hrsSimSpd.setInvertedControls(False)
-        self.hrsSimSpd.setTickPosition(QSlider.NoTicks)
-        self.hrsSimSpd.setTickInterval(1)
+        self.hrsSimSpd.setTickPosition(QSlider.TicksAbove)
+        self.hrsSimSpd.setTickInterval(15)
 
         self.vloSimSpd.addWidget(self.hrsSimSpd)
+
 
         self.vloSimSettings.addLayout(self.vloSimSpd)
 
@@ -232,8 +243,7 @@ class Ui_MainWindow(object):
         self.btnSimStart.setObjectName(u"btnSimStart")
         self.btnSimStart.setMaximumSize(QSize(23, 23))
         icon = QIcon()
-        icon.addFile(u":/icon/icons/play.png",
-                     QSize(), QIcon.Normal, QIcon.Off)
+        icon.addFile(u":/icon/icons/play.png", QSize(), QIcon.Normal, QIcon.Off)
         self.btnSimStart.setIcon(icon)
         self.btnSimStart.setIconSize(QSize(15, 15))
 
@@ -243,8 +253,7 @@ class Ui_MainWindow(object):
         self.btnSimStep.setObjectName(u"btnSimStep")
         self.btnSimStep.setMaximumSize(QSize(23, 23))
         icon1 = QIcon()
-        icon1.addFile(u":/icon/icons/step.png",
-                      QSize(), QIcon.Normal, QIcon.Off)
+        icon1.addFile(u":/icon/icons/step.png", QSize(), QIcon.Normal, QIcon.Off)
         self.btnSimStep.setIcon(icon1)
         self.btnSimStep.setIconSize(QSize(15, 15))
 
@@ -254,14 +263,15 @@ class Ui_MainWindow(object):
         self.btnSimStop.setObjectName(u"btnSimStop")
         self.btnSimStop.setMaximumSize(QSize(23, 23))
         icon2 = QIcon()
-        icon2.addFile(u":/icon/icons/stop.png",
-                      QSize(), QIcon.Normal, QIcon.Off)
+        icon2.addFile(u":/icon/icons/stop.png", QSize(), QIcon.Normal, QIcon.Off)
         self.btnSimStop.setIcon(icon2)
         self.btnSimStop.setIconSize(QSize(15, 15))
 
         self.horizontalLayout_2.addWidget(self.btnSimStop)
 
+
         self.vloSimSettings.addLayout(self.horizontalLayout_2)
+
 
         self.vloToolbar.addWidget(self.gpbSettings)
 
@@ -286,46 +296,28 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate(
-            "MainWindow", u"PyGravSim", None))
-        self.gpbBodies.setTitle(QCoreApplication.translate(
-            "MainWindow", u"\u0422\u0435\u043b\u0430", None))
-        self.gpbEdit.setTitle(QCoreApplication.translate(
-            "MainWindow", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
-        self.lblPos.setText(QCoreApplication.translate(
-            "MainWindow", u"\u041f\u043e\u043b\u043e\u0436\u0435\u043d\u0438\u0435:", None))
-        self.lblSpdX.setText(
-            QCoreApplication.translate("MainWindow", u"X:", None))
-        self.lblSpdY.setText(
-            QCoreApplication.translate("MainWindow", u"Y:", None))
-        self.lblPosY.setText(
-            QCoreApplication.translate("MainWindow", u"Y:", None))
-        self.lblPosX.setText(
-            QCoreApplication.translate("MainWindow", u"X:", None))
-        self.lblSpd.setText(QCoreApplication.translate(
-            "MainWindow", u"\u0421\u043a\u043e\u0440\u043e\u0441\u0442\u044c:", None))
-        self.lblMass.setText(QCoreApplication.translate(
-            "MainWindow", u"\u041c\u0430\u0441\u0441\u0430:", None))
-        self.lblRadius.setText(QCoreApplication.translate(
-            "MainWindow", u"\u0420\u0430\u0434\u0438\u0443\u0441:", None))
-        self.btnNew.setText(QCoreApplication.translate(
-            "MainWindow", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c", None))
-        self.btnDelete.setText(QCoreApplication.translate(
-            "MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
-        self.btnCopy.setText(QCoreApplication.translate(
-            "MainWindow", u"\u041a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
-        self.gpbSettings.setTitle(QCoreApplication.translate(
-            "MainWindow", u"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438 \u0441\u0438\u043c\u0443\u043b\u044f\u0446\u0438\u0438", None))
-        self.cbxDrawTrails.setText(QCoreApplication.translate(
-            "MainWindow", u"\u041e\u0442\u043e\u0431\u0440\u0430\u0436\u0430\u0442\u044c \u0441\u043b\u0435\u0434", None))
-        self.cbxDrawSpdVects.setText(QCoreApplication.translate(
-            "MainWindow", u"\u041e\u0442\u043e\u0431\u0440\u0430\u0436\u0430\u0442\u044c \u0432\u0435\u043a\u0442\u043e\u0440\u044b \u0441\u043a\u043e\u0440\u043e\u0441\u0442\u0435\u0439", None))
-        self.lblSimSpd.setText(QCoreApplication.translate(
-            "MainWindow", u"\u0421\u043a\u043e\u0440\u043e\u0441\u0442\u044c \u0441\u0438\u043c\u0443\u043b\u044f\u0446\u0438\u0438", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"PyGravSim", None))
+        self.gpbBodies.setTitle(QCoreApplication.translate("MainWindow", u"\u0422\u0435\u043b\u0430", None))
+        self.gpbEdit.setTitle(QCoreApplication.translate("MainWindow", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
+        self.lblPos.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u043b\u043e\u0436\u0435\u043d\u0438\u0435:", None))
+        self.lblSpdX.setText(QCoreApplication.translate("MainWindow", u"X:", None))
+        self.lblSpdY.setText(QCoreApplication.translate("MainWindow", u"Y:", None))
+        self.lblPosY.setText(QCoreApplication.translate("MainWindow", u"Y:", None))
+        self.lblPosX.setText(QCoreApplication.translate("MainWindow", u"X:", None))
+        self.lblSpd.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u043e\u0440\u043e\u0441\u0442\u044c:", None))
+        self.lblMass.setText(QCoreApplication.translate("MainWindow", u"\u041c\u0430\u0441\u0441\u0430:", None))
+        self.lblRadius.setText(QCoreApplication.translate("MainWindow", u"\u0420\u0430\u0434\u0438\u0443\u0441:", None))
+        self.btnNew.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c", None))
+        self.btnDelete.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
+        self.btnCopy.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
+        self.gpbSettings.setTitle(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438 \u0441\u0438\u043c\u0443\u043b\u044f\u0446\u0438\u0438", None))
+        self.cbxDrawTrails.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u043e\u0431\u0440\u0430\u0436\u0430\u0442\u044c \u0441\u043b\u0435\u0434", None))
+        self.cbxDrawSpdVects.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u043e\u0431\u0440\u0430\u0436\u0430\u0442\u044c \u0432\u0435\u043a\u0442\u043e\u0440\u044b \u0441\u043a\u043e\u0440\u043e\u0441\u0442\u0435\u0439", None))
+        self.lblSimSpd.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u043e\u0440\u043e\u0441\u0442\u044c \u0441\u0438\u043c\u0443\u043b\u044f\u0446\u0438\u0438", None))
         self.btnSimStart.setText("")
         self.btnSimStep.setText("")
         self.btnSimStop.setText("")
-        self.lblCreator.setText(QCoreApplication.translate(
-            "MainWindow", u"by Egor Kosachev | Distributed via MIT Liscence", None))
+        self.lblCreator.setText(QCoreApplication.translate("MainWindow", u"by Egor Kosachev | Distributed via MIT Liscence", None))
         self.lblSimulationDisplay.setText("")
     # retranslateUi
+
